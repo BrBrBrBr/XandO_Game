de6c371bb0897c4f8a63a79ff388c01e27646ace
BrBrBrBr <brinleeoaker@gmail.com>
2017-10-06 09:31:05 +0200

    Update XandO_Game.java

--- src/xando_game/XandO_Game.java
+++ src/xando_game/XandO_Game.java
@@ -13,28 +13,10 @@ import java.util.Formatter;
 import java.util.Scanner;
 
 public class XandO_Game {
-    //Things can be Optomized
-    // -> press anykey to enter
-    // -> win condition method
-    // ->
+
 
     public static void main(String[] args) {
-        /*
-        -play or veiw history(1 or 2 boolean)
-        -------play----------
-        -enter names
-        ***1st player is x second is O
-        -promt user 1 row 
-        -promt user 1 colmn
-        -promt user 2 row 
-        -promt user 2 col
-        -print board
-        -someone can win and draw can occur(define conditions)
-        -gameEnd promt to save(save to text file)
-        -promt to play again
-        ---------view Hstory--------
-        display winners and game date from txt file
-         */
+
         int WantsToPlay;
         int rounds = 1;
         Game XOGame = new Game();
